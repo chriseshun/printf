660b4cd0a6d2a97b1aaee4215b2b516acfeee7cc
chriseshun <eshunchris12@gmail.com>
2023-09-25 17:37:47 -0700

    working on printf

--- _printf.c
+++ _printf.c
@@ -9,19 +9,16 @@
 int _printf(const char *format, ...)
 {
 	unsigned int i, stringcount, count = 0;
-	
+
 	va_list args;
 
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-	
-		if (format[i] != '%')
+	if (format[i] != '%')
 		{
-		
-			our_putchar(format[i]);
-		
+
 		}
 		if (format[i] == '%' && format[i + 1] == 'c')
 		{
