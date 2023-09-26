52ebe8d298dce3ee68adf891b4030c44eed7fcf8
chriseshun <eshunchris12@gmail.com>
2023-09-25 17:58:46 -0700

    fixing the printf

--- _printf.c
+++ _printf.c
@@ -12,12 +12,17 @@ int _printf(const char *format, ...)
 
 	va_list args;
 
+	if (!format || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
 	va_start(args, format);
 
 	for (i = 0; format[i] != '\0'; i++)
 	{
-	if (format[i] != '%')
+
+		if (format[i] != '%')
 		{
+			our_putchar(format[i]);
 
 		}
 		if (format[i] == '%' && format[i + 1] == 'c')
