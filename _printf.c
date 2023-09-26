4db2a5d0376d6fa452bcd3aedf410bf20216f431
chriseshun <eshunchris12@gmail.com>
2023-09-25 18:30:08 -0700

    task

--- _printf.c
+++ _printf.c
@@ -9,45 +9,45 @@
  */
 int _printf(const char *format, ...)
 {
-	unsigned int i, stringcount, count = 0;
-
-	va_list args;
-
-	if (!format || (format[0] == '%' && format[1] == '\0'))
-		return (-1);
-
-	va_start(args, format);
-
-	for (i = 0; format[i] != '\0'; i++)
-	{
-
-		if (format[i] != '%')
-		{
-			our_putchar(format[i]);
-
-		}
-		if (format[i] == '%' && format[i + 1] == 'c')
-		{
-			our_putchar(va_arg(args, int));
-			i++;
-		}
-
-		if (format[i] == '%' && format[i + 1] == 's')
-		{
-			stringcount = my_puts(va_arg(args, char *));
-			i++;
-			count += (stringcount - 1);
-		}
-
-		if (format[i] == '%' && format[i + 1] == '%')
-		{
-			our_putchar('%');
-		}
-
-		count++;
-	}
-
-	va_end(args);
-
-	return (count);
+unsigned int i, count = 0;
+va_list args;
+if (!format)
+return (-1);
+va_start(args, format);
+for (i = 0; format[i] != '\0'; i++)
+{
+if (format[i] != '%')
+{
+our_putchar(format[i]);
+}
+else
+{
+i++;
+if (format[i] == 'c')
+{
+our_putchar(va_arg(args, int));
+}
+else if (format[i] == 's')
+{
+char *str = va_arg(args, char *);
+if (str)
+{
+int stringcount = my_puts(str);
+count += (stringcount - 1);
+}
+}
+else if (format[i] == '%')
+{
+our_putchar('%');
+}
+else
+{
+our_putchar('%');
+our_putchar(format[i]);
+}
+}
+count++;
+}
+va_end(args);
+return (count);
 }
